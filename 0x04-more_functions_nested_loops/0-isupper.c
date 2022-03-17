cf1a4bf641074b2cc5903657ce5c512d74317098
dgcp4 <ledagmawig@gmail.com>
2022-03-17 16:01:14 +0300

    fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
-* _isupper - Checks if c is uppercase character
-* @c: Character to be checked
-*
-* Return: 1 if c is uppercase, 0 otherwise
-*/
+ * _isupper
+ * @c: Character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
